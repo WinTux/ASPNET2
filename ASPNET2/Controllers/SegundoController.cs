61bfd728f4c8799e045a913b05255b9e988252ca
rusokverse <rusokverse@MACBOOKPRO>
2022-10-03 19:54:30 -0400

    Agregar archivos de proyecto.

---
+++ ASPNET2/Controllers/SegundoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET2.Controllers
+{
+    public class SegundoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tercero()
+        {
+            return View();
+        }
+    }
+}
