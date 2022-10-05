82c5d07da08757603b7dd65cbfe518281b660e9f
rusokverse <rusokverse@MACBOOKPRO>
2022-10-05 18:56:05 -0400

    Routes

---
+++ ASPNET2/Controllers/RouteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET2.Controllers
+{
+    [Route("route")]
+    public class RouteController : Controller
+    {
+        
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("obt")]
+        public IActionResult Obtener()
+        {
+            return View("ejemploRoute");
+        }
+    }
+}
