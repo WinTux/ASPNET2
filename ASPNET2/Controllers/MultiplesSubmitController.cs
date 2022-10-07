7b186c1f3be12e87d4a918236e079908c1b172aa
rusokverse <rusokverse@MACBOOKPRO>
2022-10-07 18:57:03 -0400

    Multiples submit

---
+++ ASPNET2/Controllers/MultiplesSubmitController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNET2.Controllers
+{
+    public class MultiplesSubmitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ejemplo1()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ejemplo2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ejemplo3()
+        {
+            return View();
+        }
+    }
+}
